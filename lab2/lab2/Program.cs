5a88880497ce9b6be07b26469493a051354af24b
SANG HAN <rv498@hotmail.com>
2016-12-03 16:03:42 -0800

    asdf

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 44;
+            int MaxScore = 100;
+            int score = 94;
+            decimal percent = (decimal) (score / MaxScore);
+
+
+            Console.WriteLine(age);
+
+            Console.WriteLine(percent);
+
+
+
+        }
+    }
+}
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature (Farenheit): ");
+            float originalFahrenheit = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter temperature (Celsius): ");
+            float originalCelsius = float.Parse(Console.ReadLine());
+
+            //Fahrenheit to Celsius
+            float Celsius = ((originalFahrenheit - 32) / 9) * 5;
+            float Fahrenheit = ((originalCelsius * 9) / 5) + 32;
+
+            Console.WriteLine(originalFahrenheit.ToString() + " degree Fahrenheit is " + Celsius.ToString() + " degree Celsius.");
+            Console.WriteLine(originalCelsius.ToString() + " degree Celsius is " + Fahrenheit.ToString() + " degree Fahrenheit.");
+
+
+
+        }
+    }
+}
